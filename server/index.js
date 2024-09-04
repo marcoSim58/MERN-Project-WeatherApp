730fd3f8213c5d0fc49bf51f2fc5c437dcb88ef5
babayaga47 <114303509+babayaga47@users.noreply.github.com>
2024-09-04 11:36:33 +0530

    trying different cors approach

--- server/index.js
+++ server/index.js
@@ -17,12 +17,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: "https://mern-project-weather-app.vercel.app",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://mern-project-weather-app.vercel.app",
+//     credentials: true,
+//   })
+// );
 
 app.use(
   session({
