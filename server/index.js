aeebc6ad0080ee78c7a2e23d8170828ab51f1811
babayaga47 <114303509+babayaga47@users.noreply.github.com>
2024-09-07 11:02:57 +0530

    cookie fix try 3

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(
       autoRemove: "native",
     }),
     cookie: {
-      domain: "https://mern-project-weather-app.vercel.app",
+      domain: "mern-project-weather-app.vercel.app",
       sameSite: "none",
       secure: true,
       maxAge: 1000 * 60 * 60 * 24,
