eef43fb44c2162ad1fbcd9f31a17652d814ebf18
babayaga47 <114303509+babayaga47@users.noreply.github.com>
2024-09-11 22:20:23 +0530

    "Update sameSite cookie option from 'none' to 'lax'"

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use(
     }),
     cookie: {
       domain: "mern-project-weather-app.vercel.app",
-      sameSite: "none",
+      sameSite: "lax",
       secure: true,
       maxAge: 1000 * 60 * 60 * 24,
     },
