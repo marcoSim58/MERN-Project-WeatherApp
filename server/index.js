26302c0e553ecbc9cdfe7722164f5798fa1e5518
babayaga47 <114303509+babayaga47@users.noreply.github.com>
2024-09-12 15:34:54 +0530

    expermenting to bypass cross site connection for safari browser

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(
     secret: process.env.SECRET_KEY,
 
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     proxy: true,
     store: MongoStore.create({
       mongoUrl: process.env.DATABASE_URL,
@@ -42,7 +42,7 @@ app.use(
       autoRemove: "native",
     }),
     cookie: {
-      // domain: "mern-project-weather-app.vercel.app",
+      domain: "mern-project-weather-app.vercel.app",
       sameSite: "none",
       secure: true,
       maxAge: 1000 * 60 * 60 * 24,
