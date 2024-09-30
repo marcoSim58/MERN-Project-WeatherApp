fa6ab12612f55f66d1f40092010f6a70d34dacf7
babayaga47 <114303509+babayaga47@users.noreply.github.com>
2024-09-30 20:26:50 +0530

    "Updated styles in Login and Splash1 components"

--- client/src/assets/Splash1.jsx
+++ client/src/assets/Splash1.jsx
@@ -1,11 +1,11 @@
 const Splash = () => {
   return (
     <div
-      className="bg-cover w-[100vw] h-[100vh] flex items-center justify-center"
+      className="bg-cover w-[100vw] h-[100vh] flex items-center"
       style={{ backgroundImage: "url('../../images/basepage/Splash.png')" }}>
       <div className="flex flex-col items-center w-[70%]">
         <img
-          className="w-20 pb-3"
+          className="w-20 pb-3 mt-64"
           src="../../images/basepage/WuShangWhite.png"
           alt="img1"></img>
         <img src="../../images/basepage/TenkeyohōWhite.png" alt="img2"></img>
