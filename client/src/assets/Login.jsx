9755c99422f34bf6c1daf6763e54c89d7ec0de2c
babayaga47 <114303509+babayaga47@users.noreply.github.com>
2024-10-05 18:30:33 +0530

    "Added loader CSS and removed setTimeout for data loading in Login.jsx, replaced with direct API call"

--- client/src/assets/Login.jsx
+++ client/src/assets/Login.jsx
@@ -16,49 +16,46 @@ const Login = () => {
     e.preventDefault();
     setDataLoading(true);
 
-    setTimeout(() => {
-      setDataLoading(false);
-    }, 3500);
-    // axios
-    //   .post(
-    //     `${import.meta.env.VITE_BACKEND_BASE_URL}/login`,
-    //     {
-    //       username,
-    //       password,
-    //     },
-    //     {
-    //       withCredentials: true,
-    //     }
-    //   )
-    //   .then((response) => {
-    //     // console.log(response);
-    //     if (response.status === 401) {
-    //       api.error({ message: response.data });
-    //     }
-    //     if (response.status === 500) {
-    //       api.error({ message: response.data });
-    //     }
-    //     if (response.status === 200 && typeof response.data === "object") {
-    //       console.log(response.data);
-    //       // const user = response.data;
-    //       // localStorage.setItem("user", JSON.stringify(user));
-    //       api.success({ message: "Login Sucessfull" });
-    //       setTimeout(() => {
-    //         navigate("/authenticating");
-    //         setDataLoading(false);
-    //       }, 1000);
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     if (err.response) {
-    //       console.log(err.response.data);
-    //       api.error({ message: err.response.data });
-    //     } else {
-    //       console.log(err.message);
-    //       api.error({ message: "Cannot connect to server" });
-    //     }
-    //     setDataLoading(false);
-    //   });
+    axios
+      .post(
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/login`,
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        // console.log(response);
+        if (response.status === 401) {
+          api.error({ message: response.data });
+        }
+        if (response.status === 500) {
+          api.error({ message: response.data });
+        }
+        if (response.status === 200 && typeof response.data === "object") {
+          console.log(response.data);
+          // const user = response.data;
+          // localStorage.setItem("user", JSON.stringify(user));
+          api.success({ message: "Login Sucessfull" });
+          setTimeout(() => {
+            navigate("/authenticating");
+            setDataLoading(false);
+          }, 1000);
+        }
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data);
+          api.error({ message: err.response.data });
+        } else {
+          console.log(err.message);
+          api.error({ message: "Cannot connect to server" });
+        }
+        setDataLoading(false);
+      });
   };
 
   useEffect(() => {
